6aa23239e674d7d4f7b752a3a7db845c6dae471a
Fred Heath <fred@bootstrap.me.uk>
2013-10-07 13:06:41 +0100

    added more Cucumber tests

---
+++ features/step_definitions/data_sources_steps.rb
@@ -0,0 +1,39 @@
+
+require 'nokogiri'
+require 'eventmachine'
+require 'em-http-request'
+require 'yomu'
+require 'rspec'
+require File.expand_path('../../../lib/dover_to_calais', __FILE__)
+
+
+# N.B Cucumber must be run with the Environment variable 'API_KEY' set
+# to the OpenCalais API Key value.
+
+
+Given(/^the file (\w+\.\w{3,4})$/) do |arg1|
+  puts arg1
+ @input = Dir.pwd + '/test/' + arg1
+  @output = nil
+end
+
+
+
+When(/^DoverToCalais processes this file$/) do
+  EM.run {
+
+    DoverToCalais::API_KEY =  ENV['API_KEY']
+    d1 =  DoverToCalais::Dover.new(@input)
+    d1.to_calais do |response|
+      @output = response
+      EM.stop
+    end
+
+  }
+end
+
+
+
+Then(/^the output should have no errors$/) do
+   @output.error.should be_nil
+end
---
+++ features/step_definitions/filtering_steps.rb
@@ -0,0 +1,60 @@
+require 'nokogiri'
+require 'eventmachine'
+require 'em-http-request'
+require 'yomu'
+require 'rspec'
+require File.expand_path('../../../lib/dover_to_calais', __FILE__)
+
+
+# N.B Cucumber must be run with the Environment variable 'API_KEY' set
+# to the OpenCalais API Key value.
+
+
+
+Given(/^the file '(\w+\.\w{3,4})' is successfully processed$/) do |file|
+
+  steps %{
+        Given  the file #{file}
+        When DoverToCalais processes this file
+        Then the output should have no errors
+        }
+
+end
+
+
+When(/^I filter on ({.+})/) do  |filter|
+  @filtered_output = @output.filter(eval(filter))
+
+end
+
+Then(/^the output should have (\d+) entries$/) do |item_num|
+  @filtered_output.size.should ==  item_num.to_i
+end
+
+Then(/^All entries should be named '(\w+)'$/) do  |name|
+  @filtered_output.each do |item|
+    item.name.should == name
+  end
+
+end
+
+
+And(/^All entries should have the value '(\w+)'$/) do |value|
+  @filtered_output.each do |item|
+    item.value.match(value).should_not be_nil
+  end
+end
+
+
+And(/^One entry should have the value '(\w+\s*\w+)'$/) do  |value|
+  found = false
+  @filtered_output.each do |item|
+    if item.value.match(value)
+       found =true
+      break
+    end
+  end
+
+  fail("couldn't match value '#{value}'") unless found
+end
+
